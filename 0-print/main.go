9cd19e77735d35841d77c163acb2c63cf0201d14
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-06 12:43:25 -0200

    Refactorying old solution, clean up

--- 0-print/main.go
+++ 0-print/main.go
@@ -7,16 +7,10 @@ import (
 
 func main() {
 	t := time.Now()
-	pf := fmt.Printf
-	p := fmt.Println
 
-	p("Hello, we are Holberton School")
-	pf("the date is %s\n", t.String())
-	//pf("the date is %d-%02d-%02d %d:%d:%d: \n", t.Year(), t.Month(), t.Day(), a, b, c)
-	//pf("%s\n", t.String())
-	pf("the year is %d\n", t.Year())
-	pf("the month is %s\n", t.Month())
-	pf("the day is %d\n", t.Day())
+	fmt.Println("Hello, we are Holberton School")
+	fmt.Printf("the date is %s\n", t.String())
+	fmt.Printf("the year is %d\n", t.Year())
+	fmt.Printf("the month is %s\n", t.Month())
+	fmt.Printf("the day is %d\n", t.Day())
 }
-
-// 2016-03-05 11:33:01.671605587 -0800 PST
