9a47f89ec6fb11656d1f6bac94354626b27a209a
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-06 17:48:02 -0200

    Refactor to meet requirements

--- 3-rest-client/main.go
+++ 3-rest-client/main.go
@@ -4,24 +4,30 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
 	url := "http://www.omdbapi.com/?i=tt0372784&plot=short&r=json"
-	req, err := http.Get(url)
+	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Printf("status code is %s\n", req.Status)
-
-	var decoded Movie
-	json.NewDecoder(req.Body).Decode(&decoded)
-	if err != nil {
+	var decode Movie
+	if err := json.NewDecoder(res.Body).Decode(&decode); err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Printf("The movie : %s was released in %s - the IMDB rating is %d with %s votes.\n", decoded.Title, decoded.Year, int(decoded.IMDBRating*10), decoded.IMDBVotes)
+	rating, err := strconv.ParseFloat(decode.IMDBRating, 64)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error converting str to float: %v", err)
+	}
+
+	fmt.Printf("The movie : %s was released in %s - the IMDB rating is %v%% "+
+		"with %s votes.\n", decode.Title, decode.Year,
+		rating*10, decode.IMDBVotes)
 }
