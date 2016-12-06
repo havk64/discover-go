715d747215e4fca89822f339b91170c580712ced
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-06 16:16:33 -0200

    Add top level comments with exercise requirements

--- 1-conditions-loops/main.go
+++ 1-conditions-loops/main.go
@@ -1,3 +1,27 @@
+// ===-----------------------------------------------------------------------===
+// Go fundamentals - if/else and loops
+
+// Requirements:
+// - Generate a pseudo-random integer between 0 and 100 and store it in a
+// variable called randomNumber
+
+// - Create an if/else condition to check whether your number is greater or not
+// than 50
+
+// - Store in the variable school the string Holberton School
+// - Create an if/else condition to check whether the string school is equal to
+//  Holberton School
+
+// - Store in the variable beautifulWeather the boolean value true
+
+// - Create an if/else condition to check whether the variable beautifulWeather
+// is true or not
+
+// - Store in the variable holbertonFounders the following values Rudy Rigot,
+// Sylvain Kalache, Julien Barbier
+
+// - Create a for loop that will iterate over the holbertonFounders
+// ===-----------------------------------------------------------------------===
 package main
 
 import (
