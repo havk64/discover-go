df99fbb864b5cfa78001acd9deb9ce7340fe8127
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-06 19:41:15 -0200

    Add more comments, explanations

--- 3-rest-client/main.go
+++ 3-rest-client/main.go
@@ -28,32 +28,33 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error parsing url: %v", err)
 	}
-
+	// Create http client
 	client := &http.Client{}
+	// Parametrize the query
 	form := &url.Values{
 		"i":    {"tt0372784"},
 		"plot": {"short"},
 		"r":    {"json"},
 	}
 	uri.RawQuery = form.Encode()
-
+	// Create the request
 	req, err := http.NewRequest("GET", uri.String(), nil)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "request error: %v", err)
 	}
-
+	// Make the request
 	res, err := client.Do(req)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Request error: %v\n", err)
 		return
 	}
-
+	// Parse the json results
 	var movie Movie
 	if err := json.NewDecoder(res.Body).Decode(&movie); err != nil {
 		fmt.Println(err)
 		return
 	}
-
+	// Print the requested result
 	fmt.Printf("The movie : %s was released in %v - the IMDB rating is %v%% "+
 		"with %s votes.\n", movie.Title, movie.Year,
 		int(movie.IMDBRating*10), movie.IMDBVotes)
