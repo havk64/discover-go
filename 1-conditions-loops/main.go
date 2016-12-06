89bbe84473203eb605cbb410c1e201230c7ebdf3
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-06 12:50:01 -0200

    Using Go format best practices

--- 1-conditions-loops/main.go
+++ 1-conditions-loops/main.go
@@ -6,31 +6,30 @@ import (
 )
 
 func main() {
-	//p := fmt.Println
-	pf := fmt.Printf
 	randomNumber := rand.Intn(100)
 	school := "Bad School"
 	var beautifulWeather bool
 	var holbertonFounders []string
+
 	if randomNumber > 50 {
 		school = "Holberton School"
-		pf("my random number is %d and is greater than 50\n", randomNumber)
+		fmt.Printf("my random number is %d and is greater than 50\n", randomNumber)
 	} else {
-		pf("my random number is %d and is less than 50\n", randomNumber)
+		fmt.Printf("my random number is %d and is less than 50\n", randomNumber)
 	}
-
-	pf("I am a student of the %s\n", school)
+	fmt.Printf("I am a student of the %s\n", school)
 
 	if test1(school) {
 		beautifulWeather = true
-		pf("It's a beautiful weather!\n")
+		fmt.Printf("It's a beautiful weather!\n")
 	}
 
 	if beautifulWeather == true {
 		holbertonFounders = []string{"Rudy Rigot", "Sylvain Kalache", "Julien Barbier"}
 	}
+
 	for i := 0; i < len(holbertonFounders); i++ {
-		pf("%s is a founder\n", holbertonFounders[i])
+		fmt.Printf("%s is a founder\n", holbertonFounders[i])
 	}
 }
 
