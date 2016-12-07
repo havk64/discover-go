ae553fb1c385118afd06f225f6940fe183c416b0
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-07 10:35:02 -0200

    Add comments, explanation

--- 4-sequential/main.go
+++ 4-sequential/main.go
@@ -9,40 +9,46 @@ import (
 	"os"
 )
 
+// fetchMovies is used to fetch both general movie searches and query for
+// individual movies using OMDB API
 func fetchMovies(form *url.Values, movies interface{}) {
 	uri, err := url.Parse("http://www.omdbapi.com")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error parsing url: %v", err)
 	}
-
+	// Set the client and the body form
 	client := &http.Client{}
 	uri.RawQuery = form.Encode()
+	// Buid que http request
 	req, err := http.NewRequest("GET", uri.String(), nil)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "request error: %v", err)
 	}
+	// Make que http request
 	res, err := client.Do(req)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Request error: %v\n", err)
 		os.Exit(1)
 	}
-
+	// Decode the json content using specified struct
 	if err := json.NewDecoder(res.Body).Decode(&movies); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	// Close que response body
 	defer res.Body.Close()
 }
 
 func movieSearch(search *string) {
+	// Build the query
 	form := &url.Values{
 		"s": {*search},
 	}
 
 	var movies searchMovies
-
+	// Make the query to the Api
 	fetchMovies(form, &movies)
-
+	// Iterate on the result to fetch each individual movies from the API
 	for _, v := range movies.Search {
 		form = &url.Values{
 			"i":    {v.IMDBID},
