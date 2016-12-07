a20181c173fd27911cd6440b677608ab1d84b374
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-07 10:24:50 -0200

    Add more comments, explanation

--- 4-sequential/struct.go
+++ 4-sequential/struct.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// Movie is a struct that follows the format to decode json requests to OMDB API
+// (http://www.omdbapi.com)
 type Movie struct {
 	Title      string  `json:"Title"`
 	Year       string  `json:"Year"`
@@ -25,6 +27,7 @@ type Movie struct {
 	Response   string  `json:"Response"`
 }
 
+// searchMovies is a struct to decode the query search at OMDB Api
 type searchMovies struct {
 	Search []struct {
 		Title  string
@@ -33,7 +36,8 @@ type searchMovies struct {
 	}
 }
 
-// Implementation of String method to "satisfy" Stringer interface on Movie struct
+// Implementation of String method to "satisfy" fmt Stringer interface on
+// Movie struct in order to use as default format with "fmt" package
 func (movie *Movie) String() string {
 	return fmt.Sprintf(
 		"The movie : %s was released in %v - the IMDB rating is %v%% "+
